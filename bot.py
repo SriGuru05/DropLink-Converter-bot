445036ddb61c4f1af42531e5ef1834c02a5aac51
Pdiskbot <93695686+Pdiskbot@users.noreply.github.com>
2021-12-08 17:43:41 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -27,7 +27,7 @@ bot = Client('Droplink bot',
 async def start(bot, message):
     await message.reply(
         f"**Hi {message.chat.first_name}!**\n\n"
-        "I'm Droplink bot. Just send me link and get short link made by @mrpunisher52")
+        "I'm pdiskshortforyou bot. Just send me link and get short link made by @mrpunisher52")
 
 @bot.on_message(filters.command('help') & filters.private)
 async def start(bot, message):
